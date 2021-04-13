8fd44a2d32a5d81d1958ebfd448b300f295238ac
philosofonusus <my.tentacles.are.in.you@gmail.com>
2021-04-13 16:01:03 +0300

    -axios

--- src/redux/actions/listActions/setListPage.js
+++ src/redux/actions/listActions/setListPage.js
@@ -1,10 +1,10 @@
 import {SET_LIST, SET_LIST_PAGE} from "../../types";
-import axios from 'axios'
+import request from '../../../request'
 
 const setListPage = (page) => {
     return async (dispatch, getState) => {
         const list = getState().list
-        const res = await axios.post("http://localhost:3000/products/search", {...list.listQueryParams, page, pageSize: list.pageSize})
+        const res = await request("http://localhost:3000/products/search", "POST",{...list.listQueryParams, page, pageSize: list.pageSize})
         dispatch({type: SET_LIST, payload: {val: res.data.list}})
         dispatch({type: SET_LIST_PAGE, payload: page})
     }
